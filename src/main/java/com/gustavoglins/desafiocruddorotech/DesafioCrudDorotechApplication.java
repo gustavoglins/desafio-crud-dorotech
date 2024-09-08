237d5424f43293d8ff8672c7cc5da47ac2674a83
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-09-08 15:34:27 -0400

    Initial commit

---
+++ src/main/java/com/gustavoglins/desafiocruddorotech/DesafioCrudDorotechApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoglins.desafiocruddorotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDorotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioCrudDorotechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavoglins/desafiocruddorotech/DesafioCrudDorotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoglins.desafiocruddorotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDorotechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
